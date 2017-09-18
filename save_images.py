5aa144599c78943b8c688aa8b0246819a98048cd
iwtw <wwttww@qq.com>
2017-09-18 21:24:22 +0800

    modified:   save_images.py

--- save_images.py
+++ save_images.py
@@ -1,17 +1,14 @@
 import numpy as np
 from scipy.misc import imsave
-def save_images(images_list,it,epoch):
-    "images_list : containing elements of same shape in the format of [NCHW]"
+def save_images(path , images_list,it,epoch):
+    "images_list : containing elements of same shape in the format of [NHWC]"
     m = len( images_list )
-
-    for i in range(m ):
-        images_list[i] = np.transpose(images_list[i] ,[0,3,1,2] )
     N , H , W ,C = images_list[0].shape
     rows = min( N , 5 )
     outputs = np.zeros((  H*rows,W*m,C))
     for i in range(  rows ):
         for j in range(  m ):
             outputs[  i*H : (i+1) * H , j * W : (j+1) * W ] = images_list[j][i]
-    imsave( "../training_outputs/epoch{}_it{}".format(it,epoch) , outputs  )
+    imsave( path + "/epoch{}_it{}.jpg".format(it,epoch) , outputs  )
     
     
