e2b08a6eebfcff93a649750812e39ddf441b18ec
iwtw <wwttww@qq.com>
2017-12-26 10:21:45 +0800

    modified: save_images.py

--- save_images.py
+++ save_images.py
@@ -1,16 +1,18 @@
 import numpy as np
 import os
 from scipy.misc import imsave
-def save_training_images( images_list,it,epoch, output_dir ="training_)output/" ):
+def save_training_images( images_list,epoch, output_dir ="training_)output/" ):
     "images_list : containing elements of same shape in the format of [NHWC]"
     m = len( images_list )
+    for i in range(m):
+        images_list[i] = np.array( images_list[i] )
     N , H , W ,C = images_list[0].shape
     rows = min( N , 10 )
     outputs = np.zeros((  H*rows,W*m,C))
     for i in range(  rows ):
         for j in range(  m ):
             outputs[  i*H : (i+1) * H , j * W : (j+1) * W ] = images_list[j][i]
-    imsave( output_dir + "/epoch%2d_it%d.jpg"%(epoch,it) , outputs  )
+    imsave( output_dir + "/epoch%2d.jpg"%(epoch) , outputs  )
     
 def save_images(  filename_list , images , output_dir = "output/" ):
     #假设filename_list是输入的list，且以/label/image.png的形式存储
